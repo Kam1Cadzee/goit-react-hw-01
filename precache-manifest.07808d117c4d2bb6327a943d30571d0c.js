43c931151057ffb6a21f1a10b8ed26c21b4397ff
Kam1Cadzee <39504960+Kam1Cadzee@users.noreply.github.com>
2019-04-19 16:46:41 +0300

    Updates

---
+++ precache-manifest.07808d117c4d2bb6327a943d30571d0c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "589cbfba7f0ff6233b4d",
+    "url": "/Kam1Cadzee/goit-react-hw-01/static/js/runtime~main.4adfe710.js"
+  },
+  {
+    "revision": "e7d285c9894555d43fd4",
+    "url": "/Kam1Cadzee/goit-react-hw-01/static/js/main.88ddd8ff.chunk.js"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Kam1Cadzee/goit-react-hw-01/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "34a79aca88dd21a691e8dc24a45d1706",
+    "url": "/Kam1Cadzee/goit-react-hw-01/index.html"
+  }
+];
---
+++ static/js/main.88ddd8ff.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[,,,function(e,n,o){e.exports=o(4)},function(e,n,o){"use strict";o.r(n);var t=o(0),r=o.n(t),c=o(2);o.n(c).a.render(r.a.createElement("h1",null,"Hello"),document.getElementById("root"))}],[[3,1,2]]]);
+//# sourceMappingURL=main.88ddd8ff.chunk.js.map
